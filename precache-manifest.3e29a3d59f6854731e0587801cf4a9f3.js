eaffecec734762bcd22270498790269df4a93ce1
HBtwozero <karldoudy28@gmail.com>
2020-07-18 18:18:58 -0400

    Updates

---
+++ precache-manifest.3e29a3d59f6854731e0587801cf4a9f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a487af852f97e52f2b36ae4cf3722b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e06b284dd3a3a28b1d70",
+    "url": "/static/css/main.688e862a.chunk.css"
+  },
+  {
+    "revision": "85be27c90b15d756bfea",
+    "url": "/static/js/2.133ce257.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.133ce257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06b284dd3a3a28b1d70",
+    "url": "/static/js/main.d308b290.chunk.js"
+  },
+  {
+    "revision": "c531a3433a547522141c",
+    "url": "/static/js/runtime-main.f9d4b1b7.js"
+  }
+]);
